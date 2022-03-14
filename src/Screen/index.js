fdd9bfa789239ea8560b1899f114303c8b712bf7
Romain Laurent <romain.laurent@cdiscount.com>
2022-03-14 13:44:40 +0100

    feat: 🎸 add Screen and a few styled components

---
+++ src/Screen/index.js
@@ -0,0 +1,20 @@
+import { bool, node, string } from "prop-types";
+
+import "./styles.css";
+import { Title, Subtitle } from "./styles";
+
+export default function Screen({ title, navigation, isHome = false }) {
+  return (
+    <>
+      <Title>CMyBeer</Title>
+      <Subtitle isHome={isHome}>{title}</Subtitle>
+      <nav>{navigation}</nav>
+    </>
+  );
+}
+
+Screen.propTypes = {
+  title: string.isRequired,
+  navigation: node,
+  isHome: bool,
+};
