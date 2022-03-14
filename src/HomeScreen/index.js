15c84ec9a1bc92845dfd640991cdb825ffeb8125
Romain Laurent <romain.laurent@cdiscount.com>
2022-03-14 17:32:46 +0100

    feat: 🎸 create new Beer using Card

--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -1,5 +1,14 @@
+import Beer from "../Beer";
 import Screen from "../Screen";
 
 export default function HomeScreen() {
-  return <Screen>Mon TOP 40!</Screen>;
+  const beers = [{ id: 1 }, { id: 2 }, { id: 3 }];
+
+  return (
+    <Screen>
+      {beers.map(({ id }) => (
+        <Beer key={id} id={id} />
+      ))}
+    </Screen>
+  );
 }
