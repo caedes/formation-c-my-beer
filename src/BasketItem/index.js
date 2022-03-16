93d5b96d25fc5aa00e8af500f2eb82139b62e781
Romain Laurent <romain.laurent@cdiscount.com>
2022-03-16 15:09:53 +0100

    feat: 🎸 prepare Basket with items and Price

---
+++ src/BasketItem/index.js
@@ -0,0 +1,41 @@
+import { bool, string } from "prop-types";
+import {
+  Avatar,
+  Divider,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+
+import BasketItemPrice, {
+  types as basketItemPriceTypes,
+} from "../BasketItemPrice";
+
+export default function BasketItem({
+  name,
+  imageUri,
+  price,
+  quantity,
+  divider = true,
+}) {
+  return (
+    <>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar src={imageUri} alt={name} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={name}
+          secondary={<BasketItemPrice price={price} quantity={quantity} />}
+        />
+      </ListItem>
+      {divider && <Divider variant="inset" component="li" />}
+    </>
+  );
+}
+
+BasketItem.propTypes = {
+  name: string,
+  divider: bool,
+  ...basketItemPriceTypes,
+};
